dc16a5e2c101460900165e70809479a2ad4d6a80
mzet97 <matheus.zeitune.developer@gmail.com>
2026-04-14 17:20:12 -0300

    Initial Commit

---
+++ zetssh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zetssh
+//
+//  Created by Matheus Zeitune on 14/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zetssh/zetsshApp.swift
@@ -0,0 +1,17 @@
+//
+//  zetsshApp.swift
+//  zetssh
+//
+//  Created by Matheus Zeitune on 14/04/26.
+//
+
+import SwiftUI
+
+@main
+struct zetsshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
